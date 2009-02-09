ef2a5cc25d411f2b1cc787aead922233ed02fc06
Patrick Ohly <patrick.ohly@intel.com>
2009-02-09 16:04:39 +0100

    Linux HW time stamping: define fallbacks no longer in net_tstamp.h, need them in user space to compile
    Note that the fallback are right on most platforms, but not on all!
    Better make sure that the net_tstamp.h from your Linux kernel is in
    the include search path together with platform specific asm files.

--- src/dep/ptpd_dep.h
+++ src/dep/ptpd_dep.h
@@ -29,9 +29,18 @@
 # define SO_TIMESTAMPNS 35
 #endif
 
+#ifndef SO_TIMESTAMPING
+# define SO_TIMESTAMPING 37
+#endif
+
 #ifndef SIOCGSTAMPNS
 # define SIOCGSTAMPNS 0x8907
 #endif
+
+#ifndef SIOCSHWTSTAMP
+# define SIOCSHWTSTAMP 0x89b0
+#endif
+
 #endif /* HAVE_LINUX_NET_TSTAMP_H */
 
 /**
